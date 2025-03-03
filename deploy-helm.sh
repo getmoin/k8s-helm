82f622a7df59a8ee507139bfe3106bc44450da91
getmoin <heymoinn@gmail.com>
2025-03-02 21:48:47 -0500

    removing secrets and adding env variables with --set cmd for helm

--- deploy-helm.sh
+++ deploy-helm.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
 
-# Create namespace
-sudo kubectl create namespace credential-showcase-ns --dry-run=client -o yaml | sudo kubectl apply -f -
-sudo kubectl create namespace showcase-ui-ns --dry-run=client -o yaml | sudo kubectl apply -f -
-
-# Create secrets with credentials
-sudo kubectl create secret generic credential-showcase \
-    -n credential-showcase-ns \
-    --from-literal=API_KEY="$HELLO_WORLD_API_KEY" \
-    --from-literal=AUTH_TOKEN="$HELLO_WORLD_AUTH_TOKEN" \
-    --from-literal=WEATHER_API_KEY="$WEATHER_API_KEY" \
-    --from-literal=DB_PASSWORD="$DB_PASSWORD" \
-    --from-literal=RABBIT_PASSWORD="$RABBIT_PASSWORD" \
-    --dry-run=client -o yaml | sudo kubectl apply -f -
-sudo kubectl create secret generic showcase-ui \
-    -n showcase-ui-ns \
-    --from-literal=BACKEND_URL="$BACKEND_URL" \
-    --from-literal=AUTH_TOKEN="$UI_AUTH_TOKEN" \
-    --dry-run=client -o yaml | sudo kubectl apply -f -
-
 # Add repositories and update dependencies
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 cd credential-showcase
 helm dependency update
 cd ..
 
-
 # Deploy applications
-helm upgrade --install credential-showcase ./credential-showcase -f ./credential-showcase/dev-values.yaml
+# helm upgrade --install credential-showcase ./credential-showcase -f ./credential-showcase/dev-values.yaml
+
+helm upgrade --install credential-showcase ./credential-showcase \
+  -f ./credential-showcase/dev-values.yaml \
+  --set services[0].env.API_KEY="$HELLO_WORLD_API_KEY" \
+  --set services[0].env.AUTH_TOKEN="$HELLO_WORLD_AUTH_TOKEN" \
+  --set services[1].env.WEATHER_API_KEY="$WEATHER_API_KEY" \
+  --set services[1].env.DB_PASSWORD="$DB_PASSWORD" \
+  --set services[1].env.RABBIT_PASSWORD="$RABBIT_PASSWORD" \
+  --set postgresql.auth.password="$DB_PASSWORD" \
+  --set rabbitmq.auth.password="$RABBIT_PASSWORD"
 
 helm upgrade --install showcase-ui ./showcase-ui -f ./showcase-ui/dev-values.yaml
+  --set services[0].env.BACKEND_API_KEY="$BACKEND_API_KEY" \
+  --set services[0].env.AUTH_TOKEN="$UI_AUTH_TOKEN"
 
 # helm upgrade --install prometheus prometheus-community/prometheus \
 #   --set server.resources.limits.cpu=100m \
